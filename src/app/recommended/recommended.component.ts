b62fdcf88c250d5163fe09f9c5a9b36e30453073
kamakozi <ziga.zoric@gmail.com>
2025-04-05 14:04:14 +0200

    First commit. Adeed item.component section, added recommended.component section.

---
+++ src/app/recommended/recommended.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recommended',
+  imports: [],
+  templateUrl: './recommended.component.html',
+  styleUrl: './recommended.component.css'
+})
+export class RecommendedComponent {
+
+}
